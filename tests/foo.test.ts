6f0e1d3939b35bfb56ecfe7c27404860f88e1223
Ethan Zhang <ethan.zhang@ringcentral.com>
2021-10-18 12:21:12 +0800

    update

--- tests/foo.test.ts
+++ tests/foo.test.ts
@@ -4,4 +4,20 @@ describe('Test range.', () => {
   test('Test range(1, 10).', async () => {
     expect(range(1, 10)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
+
+  test('[linux,mac,win] Test 1.', async () => {
+    expect(1).toEqual(1);
+  });
+
+  test('[mac] Test 2.', async () => {
+    expect(1).toEqual(1);
+  });
+
+  test('[win] Test 3.', async () => {
+    expect(1).toEqual(1);
+  });
+
+  test('[linux,mac] Test 4.', async () => {
+    expect(1).toEqual(1);
+  });
 });
